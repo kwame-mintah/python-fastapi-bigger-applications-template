a45220f6367465c4d8cff4989175b2b9376f75db
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-10-13 12:54:45 +0100

    test(init): initial unit tests added for fastapi routes
    unit tests to verify endpoints return expected values

---
+++ tests/test_demo.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_get_demo_root():
+    response = client.get("/dashboard")
+    assert response.status_code == 200
+    assert response.json() == {
+        "messages": 1,
+        "example": {
+            "placeholder": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+            "ut labore et dolore magna aliqua."
+        },
+    }
---
+++ tests/test_versions.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_get_python_version():
+    response = client.get("/version/python")
+    assert response.status_code == 200
+    assert (
+        response.json()
+        == "sys.version_info(major=3, minor=9, micro=6, releaselevel='final', serial=0)"
+    )
+
+
+def test_get_fastapi_version():
+    response = client.get("/version/fastapi")
+    assert response.status_code == 200
+    assert response.json() >= "0.103.2"
