bfcb39d30ed4e48219040e349fb33773f09e94ed
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-10-13 17:46:38 +0100

    refactor(main): remove auto reload of fastapi
    removed `reload` as this was used during local development.

--- app/main.py
+++ app/main.py
@@ -15,4 +15,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
