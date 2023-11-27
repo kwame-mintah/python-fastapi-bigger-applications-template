697411346a2525df187db61f32d9e8c0987413b6
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-11-27 17:53:34 +0000

    feat(pydantic): introduce pydantic models application
    example models added `Message` and `Package` to be used as response models for existing endpoints

---
+++ app/models/models.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+
+
+class Example(BaseModel):
+    placeholder: str = Field(None)
+
+
+class Message(BaseModel):
+    messages: int = Field(None, title="Message ID")
+    example: Example = Field(None)
+
+
+class Package(BaseModel):
+    version: str = Field(None, title="Package version")
