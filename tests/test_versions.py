64800e445a16ba4bc013bdbc74431e39f916fa1b
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-11-27 17:56:37 +0000

    test(versions): update expected response for endpoints
    endpoints now use a pydantic model so slight change to json response

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -8,13 +8,13 @@ client = TestClient(app)
 def test_get_python_version():
     response = client.get("/version/python")
     assert response.status_code == 200
-    assert (
-        response.json()
-        == "sys.version_info(major=3, minor=11, micro=5, releaselevel='final', serial=0)"
-    )
+    assert response.json() == {
+        "version": "sys.version_info(major=3, minor=11, micro=5, "
+        "releaselevel='final', serial=0)"
+    }
 
 
 def test_get_fastapi_version():
     response = client.get("/version/fastapi")
     assert response.status_code == 200
-    assert response.json() >= "0.103.2"
+    assert response.json()["version"] >= "0.103.2"
