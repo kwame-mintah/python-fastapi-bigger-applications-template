77b091b0bc6c4bb87c473018f069b532be0659dc
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-05-07 20:57:55 +0100

    docs(get_demo_service): add doc strings to functions
    provide description for functions and expected params

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,11 @@
+from typing import Type
+
 from app.services.demo_service import DemoService
 
 
-def get_demo_service():
+def get_demo_service() -> Type[DemoService]:
+    """
+    Dependency injection example.
+    :return: DemoService
+    """
     return DemoService
