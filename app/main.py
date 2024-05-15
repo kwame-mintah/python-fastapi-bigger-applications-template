fadc2f815b92e72560da439f9045abeee0c8693f
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-05-15 21:12:13 +0100

    feat(main): include meta data for fastapi
    example meta deta provided for template as per spec

--- app/main.py
+++ app/main.py
@@ -1,19 +1,33 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from app.internal import versions
 from app.routers import demo
 
-app = FastAPI()
+# Provide meta data for API.
+# https://fastapi.tiangolo.com/tutorial/metadata/#metadata-for-api
+app = FastAPI(
+    title="FastAPI Bigger Applications Template",
+    description="An example project for bigger applications",
+    contact={
+        "name": "Template",
+        "url": "https://github.com/kwame-mintah/python-fastapi-bigger-applications-template",
+        "email": "email@email.com",
+    },
+    license_info={
+        "name": "Example License",
+        "url": "https://choosealicense.com/",
+    },
+)
 app.include_router(demo.router)
 app.include_router(versions.router)
 
 
-@app.get("/")
+@app.get(path="/", tags=["root"], status_code=status.HTTP_200_OK)
 async def root() -> dict:
     """
     Example response, when accessing root e.g. `http://localhost:8000/`
-    :return: Message
+    :return: response
     """
     return {"message": "What a wonderful kind of day."}
 
