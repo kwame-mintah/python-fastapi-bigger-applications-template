f646edb487dd656e2c69560e7c527a1661136ce4
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-05-15 21:08:52 +0100

    refactor(models): provide titles for example modes and change defaults
    `Example` and `Message` no longer a default of `None` value must be provided. updated `Package` to have a default of empty string

--- app/models/models.py
+++ app/models/models.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel, Field
 
 
 class Example(BaseModel):
-    placeholder: str = Field(None)
+    placeholder: str = Field(title="Example message returned")
 
 
 class Message(BaseModel):
     messageId: int = Field(title="Message ID", examples=[1])
-    example: Example = Field(None)
+    example: Example = Field(title="Example Model inheriting another model")
 
 
 class Package(BaseModel):
-    version: str = Field(None, title="Package version", examples=["0.109.0"])
+    version: str = Field(default="", title="Package version", examples=["0.109.0"])
