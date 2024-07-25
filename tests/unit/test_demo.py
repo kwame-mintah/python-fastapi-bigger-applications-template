ea7fd453e1caf69fcf22e68aa24f0ad1f782c8d7
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-07-25 21:52:23 +0100

    test(demo): add new unit test for post requests and update existing get request test
    `test_post_demo_root` added to check that response contains previous entry and new entry added via post request. updated `test_get_demo_root` response to be a list to reflect latest changes

--- tests/unit/test_demo.py
+++ tests/unit/test_demo.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from app.main import app
+from app.models.models import Message, Example
 
 client = TestClient(app)
 
@@ -8,10 +9,33 @@ client = TestClient(app)
 def test_get_demo_root() -> None:
     response = client.get("/demo")
     assert response.status_code == 200
-    assert response.json() == {
-        "messageId": 1,
-        "example": {
-            "placeholder": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
-            "ut labore et dolore magna aliqua."
+    assert response.json() == [
+        {
+            "messageId": 1,
+            "example": {
+                "placeholder": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+                "ut labore et dolore magna aliqua."
+            },
+        }
+    ]
+
+
+def test_post_demo_root() -> None:
+    body = Message(
+        messageId=2, example=Example(placeholder="Unit Testing")
+    ).model_dump_json()
+    response = client.post(
+        url="/demo", data=body, headers={"Content-type": "application/json"}
+    )
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "example": {
+                "placeholder": "Lorem ipsum dolor sit amet, consectetur "
+                "adipiscing elit, sed do eiusmod tempor "
+                "incididuntut labore et dolore magna aliqua."
+            },
+            "messageId": 1,
         },
-    }
+        {"example": {"placeholder": "Unit Testing"}, "messageId": 2},
+    ]
