b9c64fd537580f26aa28cd24b1c90fb9fc1e2744
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-02-09 12:30:17 +0000

    test(TestClient): provide example for using dependency overrides
    use dependency override for unit test `test_get_demo_root` and add fixture to remove dependency overrides applied

--- tests/unit/test_demo.py
+++ tests/unit/test_demo.py
@@ -1,27 +1,64 @@
+from typing import Generator
+from unittest.mock import Mock, patch
+
+import pytest
 from fastapi.testclient import TestClient
 
+from app.dependencies import get_demo_service
 from app.main import app
 from app.models.models import Message, Example
 
-client = TestClient(app)
+
+@pytest.fixture(scope="function")
+def client() -> Generator:
+    """
+    Provide a TestClient that can override any dependency
+    overrides set.
+    :return: TestClient
+    """
+    with TestClient(app) as client:
+        app.dependency_overrides.clear()
+        yield client
 
 
-def test_get_demo_root() -> None:
-    response = client.get("/demo")
+@pytest.fixture(scope="function", autouse=False)
+def mock_dependency_client() -> TestClient:
+    """
+    Provide a TestClient that has dependency overrides as an example.
+    Overrides `DemoService` as an example.
+    :return: TestClient
+    """
+
+    def mock_example_function() -> Mock:
+        """
+        Override function and return a specific value.
+        :return: Mock
+        """
+        mock_demo_service = Mock()
+        mock_demo_service.return_stub_data.return_value = [
+            Message(
+                messageId=1,
+                example=Example(placeholder="Example mocked dependency override."),
+            )
+        ]
+        return mock_demo_service
+
+    app.dependency_overrides.update({get_demo_service: mock_example_function})
+    return TestClient(app=app)
+
+
+def test_get_demo_root(mock_dependency_client: TestClient) -> None:
+    response = mock_dependency_client.get("/demo")
     assert response.status_code == 200
     assert response.json() == [
         {
             "messageId": 1,
-            "example": {
-                "placeholder": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-                "sed do eiusmod tempor incididunt"
-                "ut labore et dolore magna aliqua."
-            },
+            "example": {"placeholder": "Example mocked dependency override."},
         }
     ]
 
 
-def test_post_demo_root() -> None:
+def test_post_demo_root(client: TestClient) -> None:
     body = Message(
         messageId=2, example=Example(placeholder="Unit Testing")
     ).model_dump_json()
